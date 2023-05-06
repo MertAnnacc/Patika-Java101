19d36cc227191da9468cb50f44024583280def75
MertAnnacc <mertannac91@gmail.com>
2023-05-06 16:02:21 +0300

    5

--- src/main/java/patika/C14_ArtikYil.java
+++ src/main/java/patika/C14_ArtikYil.java
@@ -10,10 +10,15 @@ public class C14_ArtikYil {
         System.out.println("Yılı giriniz :");
         yil = scan.nextInt();
 
-        if (yil % 4 == 0 && yil % 400 !=0){
+        if (yil % 400 == 0){
             System.out.println(yil + " bir artık yıldır !");
         } else {
-            System.out.println(yil + " bir artık yıl değildir !");
+            if (yil % 100 != 0 && yil % 4 == 0){
+                System.out.println(yil + " bir artık yıldır !");
+            }else {
+                System.out.println(yil + " bir artık yıl değildir !");
+            }
+
 
         }
 
