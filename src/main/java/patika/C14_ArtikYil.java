ffe64d008741dbb861e81e787a864c0c5e7bf8aa
MertAnnacc <mertannac91@gmail.com>
2023-05-06 15:54:07 +0300

    4

---
+++ src/main/java/patika/C14_ArtikYil.java
@@ -0,0 +1,21 @@
+package patika;
+
+import java.util.Scanner;
+
+public class C14_ArtikYil {
+    public static void main(String[] args) {
+        int yil;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Yılı giriniz :");
+        yil = scan.nextInt();
+
+        if (yil % 4 == 0 && yil % 400 !=0){
+            System.out.println(yil + " bir artık yıldır !");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir !");
+
+        }
+
+    }
+}
